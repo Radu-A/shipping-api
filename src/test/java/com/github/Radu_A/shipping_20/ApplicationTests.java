de8a3afcef03e87d0639ac69df0a1d20c9b8d64a
Radu-A <elcastravete@gmail.com>
2026-05-05 17:47:18 +0200

    chore: created file structure

---
+++ src/test/java/com/github/Radu_A/shipping_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.shipping_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
